48f6b53f24c92317452d09d28c86ba131e0c2549
FabioGVL <135769523+FabioGVL@users.noreply.github.com>
2026-09-23 14:10:43 -0300

    SwagLabsAutomation

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,26 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  env: {
+    usuario: "standard_user",
+    senha: "secret_sauce",
+
+    usuario_invalido: "invalid_user",
+    usuario_invalido2: "standard_user123",
+    usuario_maiusculo: "STANDARD_USER",
+
+    senha_invalida: "invalid_password",
+    senha_invalida2: "secret_sauce123",
+    senha_invalida3: "secret",
+    senha_maiusculo: "SECRET_SAUCE"
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    "chromeWebSecurity": false,
-    "baseUrl": "http://www.saucedemo.com"
+
+    chromeWebSecurity: false,
+    baseUrl: "https://www.saucedemo.com"
   },
 });
